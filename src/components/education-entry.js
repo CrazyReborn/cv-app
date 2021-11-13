c17c216718e32aff5f38cb842d79300601cea02c
Vladislav Kuznetsov <vladkuzodin@gmail.com>
2021-11-13 11:11:04 +0100

    now is a function component.

--- src/components/education-entry.js
+++ src/components/education-entry.js
@@ -1,79 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import uniqid from "uniqid";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPen, faMinus } from '@fortawesome/free-solid-svg-icons';
 
-class EducationEntry extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            startYear: '',
-            endYear: '',
-            uniName: '',
-            title: '',
-            editing: false,
-            id: uniqid(),
+function EducationEntry(props) {
+
+    const [startYear, setStartYear] = useState('');
+    const [endYear, setEndYear] = useState('');
+    const [uniName, setUniName] = useState('');
+    const [title, setTitle] = useState('');
+    const [editing, setEditing] = useState('');
+    const [id, setId] = useState(uniqid());
+
+    let newEdu = {};
+
+    function handleEdit(edu) {
+        setStartYear(edu.startYear);
+        setEndYear(edu.endYear);
+        setUniName(edu.uniName);
+        setTitle(edu.title);
+        setEditing(!editing);
+        setId(edu.id);
+    }
+
+    function changeStateToDefault() {
+        setStartYear('');
+        setEndYear('');
+        setUniName('');
+        setTitle('');
+        setEditing(false);
+        setId(uniqid());
+    }
+    
+    function createNewEdu() {
+        newEdu = {
+            startYear,
+            endYear,
+            uniName,
+            title,
+            editing,
+            id,
         }
-        this.handleChange = this.handleChange.bind(this);
-        this.changeStateToDefault = this.changeStateToDefault.bind(this);
-        this.handleEdit = this.handleEdit.bind(this);
     }
-    handleEdit(edu) {
-        
-        this.setState({
-            startYear: edu.startYear,
-            endYear: edu.endYear,
-            uniName: edu.uniName,
-            title: edu.title,
-            editing: !this.state.editing,
-            id: edu.id
-        },);
-    }
-    handleChange(e) {
-        const name = e.target.name;
-        const value = e.target.value;
-        this.setState({
-            [name]: value,
-        });
-    }
-    changeStateToDefault() {
-        this.setState({
-            startYear: '',
-            endYear: '',
-            uniName: '',
-            title: '',
-            editing: false,
-            id: uniqid(),
-        });
-    }
-    render() {
-        const { edu, handleResubmit, handleDelete } = this.props;
-        const { startYear, endYear, uniName, title } = this.state;
-        return (
-            <div>
-                {!this.state.editing
-                    ? <div>
+
+
+    const { edu, handleResubmit, handleDelete } = props;
+    return (
+        <div>
+            {!editing
+                ? <div>
                     <h3>{edu.uniName}</h3>
                     <p>Title: {edu.title}</p>
                     <p>From {edu.startYear} to {edu.endYear}</p>
-                    <button onClick={() => this.handleEdit(edu)}><FontAwesomeIcon icon={faPen}/> Edit</button>
-                    <button onClick={() => handleDelete(edu.id)}><FontAwesomeIcon icon={faMinus}/> Delete</button>
-                    </div>
-                    : <form className="education" onSubmit={(e) => {handleResubmit(e, edu, this.state); this.changeStateToDefault()}}>
-                        <label htmlFor="startYear">Start Year</label>
-                        <input onChange={this.handleChange} id="startYear" name="startYear" type="number" min="1960" max="2021" step="1" value={startYear}></input>
-                        <label htmlFor="endYear">End Year</label>
-                        <input onChange={this.handleChange} id="endYear" name="endYear" type="number" min="1960" max="2021" step="1" value={endYear}></input>
-                        <label htmlFor="uniName">School Name</label>
-                        <input onChange={this.handleChange} type="text" name="uniName" id="uniName" value={uniName}></input>
-                        <label htmlFor="title">Title</label>
-                        <input onChange={this.handleChange} type="text" name="title" id="title" value={title}></input>
-                        <input  type="submit"></input>
-                        </form>
-                        }
-            </div>
-        )
-    }
+                    <button onClick={() => handleEdit(edu)}><FontAwesomeIcon icon={faPen} /> Edit</button>
+                    <button onClick={() => handleDelete(edu.id)}><FontAwesomeIcon icon={faMinus} /> Delete</button>
+                </div>
+                : <form className="education" onSubmit={(e) => { createNewEdu(); handleResubmit(e, edu, newEdu); changeStateToDefault() }}>
+                    <label htmlFor="startYear">Start Year</label>
+                    <input onChange={(e) => setStartYear(e.target.value)} id="startYear" name="startYear" type="number" min="1960" max="2021" step="1" value={startYear}></input>
+                    <label htmlFor="endYear">End Year</label>
+                    <input onChange={(e) => setEndYear(e.target.value)} id="endYear" name="endYear" type="number" min="1960" max="2021" step="1" value={endYear}></input>
+                    <label htmlFor="uniName">School Name</label>
+                    <input onChange={(e) => setUniName(e.target.value)} type="text" name="uniName" id="uniName" value={uniName}></input>
+                    <label htmlFor="title">Title</label>
+                    <input onChange={(e) => setTitle(e.target.value)} type="text" name="title" id="title" value={title}></input>
+                    <input type="submit"></input>
+                </form>
+            }
+        </div>
+    )
 }
 
 export default EducationEntry
